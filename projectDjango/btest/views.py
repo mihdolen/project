d558b4b95cac4624821fd4b86d1b4946098f49d2
mihdolen <mishlad@mail.ru>
2023-11-20 17:48:20 +0300

    little changes

--- projectDjango/btest/views.py
+++ projectDjango/btest/views.py
@@ -4,14 +4,10 @@ from django.db.models import Q
 from django.shortcuts import render
 
 def index(request):
-  s1 = 'Список участий\r\n\r\n\r\n'
+
   wOns=workingOn.objects.select_related("project").select_related("worker")
-
-
-  for bb in workingOn.objects.select_related("project").select_related("worker"):
-    s1 += str(bb.project.title) +'  '+ bb.worker.surname +  '\r\n'
-  s2 = '\r\n\r\n\r\n\r\nСписок проектов\r\n\r\n'
   dprojects={}
+  
   for bb in workingOn.objects.select_related("project").select_related("worker"):
     if(bb.project.title in dprojects.keys()):
         dprojects[bb.project.title]+=[bb.worker.surname + " " +  bb.worker.name + " " + bb.worker.otchestvo]
